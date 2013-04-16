770ce7a2455b7b0a23fd3081801a72c51c3f5bab
Sebastian Erdweg <seba.github@gmail.com>
2013-04-16 11:42:39 +0200

    make sweet tooth self-contained: binaries for Stratego and SugarJ

--- sweet-tooth.sh
+++ sweet-tooth.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+# example call
+#   ./sweet-tooth.sh  ~/projects/sugarj/case-studies/java-pet-store/src/java  xml/Sugar.sugj  desugar-xml
+
 SRCPATH=$1
 SRCFILE=$2
 DESUGARING=$3
 
 DESUGARING_NORM=`echo $DESUGARING | sed -e "s/\\-/\\_/g"`
 SCRIPTDIR=$(dirname $0)
-SUGARJ_BASE=/Users/seba/projects/sugarj
-SUGARJ=$SUGARJ_BASE/deployment/cli-scripts/sugarj-dev
-SUGARJ_STDLIB=$SUGARJ_BASE/stdlib
-SUGARJ_JAVALIB=$SUGARJ_BASE/language-libraries/java/src
-STRATEGO_JAR=/Users/seba/Library/eclipse-sugarj/plugins/org.strategoxt.strj_0.17.92.201212201448/java/strategoxt.jar
+LIB=$SCRIPTDIR/lib
+SUGARJ_BASE=$LIB/sugarj
+SUGARJ=$SUGARJ_BASE/bin/sugarj
+SUGARJ_LIB=$SUGARJ_BASE/lib
+STRATEGO_JAR=$LIB/strategoxt.jar
 STRJ="java -jar $STRATEGO_JAR"
 
 echo "compile sweet tooth Stratego programs"
 mkdir -p $SCRIPTDIR/bin
-$STRJ -o $SCRIPTDIR/bin/extract_generation_type.java -i $SCRIPTDIR/stratego/extract-generation-type.str -I $SUGARJ_STDLIB
+$STRJ -o $SCRIPTDIR/bin/extract_generation_type.java -i $SCRIPTDIR/stratego/extract-generation-type.str -I $SUGARJ_LIB
 javac -cp $SCRIPTDIR/bin:$STRATEGO_JAR -d $SCRIPTDIR/bin $SCRIPTDIR/bin/*.java
 echo
 
@@ -29,7 +32,7 @@ echo
 
 echo "normalize resulting Stratego file"
 NORM=`mktemp -t desugar.str`
-$STRJ -F -o $NORM -i $SUGARJ_BIN/`dirname $SRCFILE`/`basename $SRCFILE .sugj`.str -I $SUGARJ_BIN -I $SUGARJ_STDLIB -I $SUGARJ_JAVALIB
+$STRJ -F -o $NORM -i $SUGARJ_BIN/`dirname $SRCFILE`/`basename $SRCFILE .sugj`.str -I $SUGARJ_BIN -I $SUGARJ_LIB -I $SUGARJ_LIB
 echo "Wrote normalized Stratego code"
 echo "  $NORM"
 echo

74ba43d9ee79401343e23e6536a7f3dd4aabcd83
Sebastian Erdweg <seba.github@gmail.com>
2014-07-30 11:49:05 +0200

    update lib

--- sweet-tooth.sh
+++ sweet-tooth.sh
@@ -3,11 +3,10 @@
 # example call
 #   ./sweet-tooth.sh  ~/projects/sugarj/case-studies/java-pet-store/src/java  xml/Sugar.sugj  desugar-xml
 
-SRCPATH=$1
+ProjectPATH=$1
+SUGARJ_BIN=$ProjectPATH/bin
 SRCFILE=$2
 DESUGARING=$3
-MATCHPATH=$4
-MATCHFILE=$5
 
 DESUGARING_NORM=`echo $DESUGARING | sed -e "s/\\-/\\_/g"`
 SCRIPTDIR=$(dirname $0)
@@ -18,20 +17,19 @@ SUGARJ_LIB=$SUGARJ_BASE/lib
 STRATEGO_JAR=$LIB/strategoxt.jar
 STRJ="java -jar $STRATEGO_JAR"
 
-echo "compile sweet tooth Stratego programs"
-mkdir -p $SCRIPTDIR/bin
-$STRJ -o $SCRIPTDIR/bin/extract_generation_type.java -i $SCRIPTDIR/stratego/extract-generation-type.str -I $SUGARJ_LIB
-$STRJ -m matching-main -o $SCRIPTDIR/bin/matching.java -i $SCRIPTDIR/stratego/matching.str -I $SUGARJ_LIB -la stratego-xtc -la stratego-sglr
-javac -cp $SCRIPTDIR/bin:$STRATEGO_JAR -d $SCRIPTDIR/bin $SCRIPTDIR/bin/*.java
-echo
+# echo "compile sweet tooth Stratego programs"
+# mkdir -p $SCRIPTDIR/bin
+# $STRJ -o $SCRIPTDIR/bin/extract_generation_type.java -i $SCRIPTDIR/stratego/extract-generation-type.str -I $SUGARJ_LIB
+# $STRJ -m matching-main -o $SCRIPTDIR/bin/matching.java -i $SCRIPTDIR/stratego/matching.str -I $SUGARJ_LIB -la stratego-xtc -la stratego-sglr
+# javac -cp $SCRIPTDIR/bin:$STRATEGO_JAR -d $SCRIPTDIR/bin $SCRIPTDIR/bin/*.java
+# echo
 
-echo "compile sugarj module"
-SUGARJ_BIN=/tmp/sugarj-out
-SUGARJ_CACHE=/tmp/sugarj-cache
-mktemp -d -q /tmp/sugarj-out > /dev/null 
-mktemp -d -q /tmp/sugarj-cache > /dev/null 
-$SUGARJ --cache $SUGARJ_CACHE -l java -d $SUGARJ_BIN --sourcepath $SRCPATH $SRCFILE
-echo
+# echo "compile sugarj module"
+# SUGARJ_CACHE=/tmp/sugarj-cache
+# mktemp -d -q /tmp/sugarj-out > /dev/null 
+# mktemp -d -q /tmp/sugarj-cache > /dev/null 
+# $SUGARJ --cache $SUGARJ_CACHE -l java -d $SUGARJ_BIN --sourcepath $ProjectPATH/src $SRCFILE
+# echo
 
 echo "normalize resulting Stratego file"
 NORM=`mktemp -t desugar-XXXXX.str`
@@ -46,21 +44,21 @@ java -Xmx2048m -cp $SCRIPTDIR/bin:$STRATEGO_JAR extract_generation_type -i $NORM
 cat $TYPEFILE
 echo
 
-echo "match pattern against target file" 
-MATCH_PAIR=`mktemp -t match-pair-XXXXX.aterm`
-#$SUGARJ --cache $SUGARJ_CACHE -l java -d $SUGARJ_BIN --sourcepath $MATCHPATH $MATCHFILE
-Name1=`basename $MATCHFILE .sugj`
-Name2=`basename $Name1 .java`
-MODEL=$SUGARJ_BIN/`dirname $MATCHFILE`/$Name2.model
+# echo "match pattern against target file" 
+# MATCH_PAIR=`mktemp -t match-pair-XXXXX.aterm`
+# #$SUGARJ --cache $SUGARJ_CACHE -l java -d $SUGARJ_BIN --sourcepath $MATCHPATH $MATCHFILE
+# Name1=`basename $MATCHFILE .sugj`
+# Name2=`basename $Name1 .java`
+# MODEL=$SUGARJ_BIN/`dirname $MATCHFILE`/$Name2.model
 
-echo "Use model $MODEL"
+# echo "Use model $MODEL"
 
-echo "(" > $MATCH_PAIR
-cat $TYPEFILE >> $MATCH_PAIR
-echo "," >> $MATCH_PAIR
-cat $MODEL >> $MATCH_PAIR
-echo ")" >> $MATCH_PAIR
+# echo "(" > $MATCH_PAIR
+# cat $TYPEFILE >> $MATCH_PAIR
+# echo "," >> $MATCH_PAIR
+# cat $MODEL >> $MATCH_PAIR
+# echo ")" >> $MATCH_PAIR
 
-java -cp $SCRIPTDIR/bin:$STRATEGO_JAR matching -i $MATCH_PAIR
-echo
+# java -cp $SCRIPTDIR/bin:$STRATEGO_JAR matching -i $MATCH_PAIR
+# echo
 

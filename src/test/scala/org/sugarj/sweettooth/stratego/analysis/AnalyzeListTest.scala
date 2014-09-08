4504c90d4a3fc3ebd3f61e81a703323b4f838599
Sebastian Erdweg <seba.github@gmail.com>
2014-09-08 15:53:27 +0200

    analyze map

--- src/test/scala/org/sugarj/sweettooth/stratego/analysis/AnalyzeListTest.scala
+++ src/test/scala/org/sugarj/sweettooth/stratego/analysis/AnalyzeListTest.scala
@@ -22,7 +22,7 @@ class AnalyzeListTest extends FunSuite {
     if (n == 0)
       scala.List()
     else
-      Trm.App(Symbol(s"Foo$n"))::mkListOfLength(n-1)
+      Trm.App(Symbol(s"Elem_${n-1}"))::mkListOfLength(n-1)
 
   def assertDomT(expected: dom.T)(actual: dom.T) = assertResult(expected)(actual)
 
@@ -39,16 +39,53 @@ class AnalyzeListTest extends FunSuite {
   test("cons") {
     for (i <- 1 to 20) {
       val l = mkListOfLength(i)
-      assertDomT(lift(Trm.App('Foo)::l))(analysis.analyze(Call('cons), lift(Trm.App('_, Trm.App('Foo), l)), DEFS))
+      assertDomT(
+        lift(Trm.App('Foo)::l))(
+        analysis.analyze(Call('cons), lift(Trm.App('_, Trm.App('Foo), l)), DEFS))
     }
+  }
+
+  test("cons top nil") {
+    assertDomT(
+      dom.liftApp('Cons, dom.top, dom.liftApp('Nil)))(
+      analysis.analyze(Call('cons), dom.liftApp('_, dom.top, dom.liftApp('Nil)), DEFS))
+  }
+
+  test("cons Zero top") {
+    assertDomT(
+      dom.liftApp('Cons, dom.liftApp('Nil), dom.top))(
+      analysis.analyze(Call('cons), dom.liftApp('_, dom.liftApp('Nil), dom.top), DEFS))
+  }
+
+  test("pair to list") {
+    val pairToList = Seqs(
+      ??('_@@('x, 'y)),
+      !!('_@@('y, 'Nil@@())),
+      Call('cons),
+      ??('xs),
+      !!('_@@('x, 'xs)),
+      Call('cons))
+
+    assertDomT(
+      dom.liftApp('Cons, dom.top, dom.liftApp('Cons, dom.top, dom.liftApp('Nil))))(
+      analysis.analyze(pairToList, dom.liftApp('_, dom.top, dom.top), DEFS))
+    assertDomT(
+      dom.liftApp('Cons, dom.top, dom.liftApp('Cons, dom.liftApp('Zero), dom.liftApp('Nil))))(
+      analysis.analyze(pairToList, dom.liftApp('_, dom.top, dom.liftApp('Zero)), DEFS))
+    assertDomT(
+      dom.liftApp('Cons, dom.liftApp('Zero), dom.liftApp('Cons, dom.top, dom.liftApp('Nil))))(
+      analysis.analyze(pairToList, dom.liftApp('_, dom.liftApp('Zero), dom.top), DEFS))
+    assertDomT(
+      dom.liftApp('Cons, dom.liftApp('Zero), dom.liftApp('Cons, dom.liftApp('One), dom.liftApp('Nil))))(
+      analysis.analyze(pairToList, dom.liftApp('_, dom.liftApp('Zero), dom.liftApp('One)), DEFS))
   }
 
   test(s"map") {
     for (i <- 1 to 20) {
       val l = mkListOfLength(i)
-      val s = Build('Zero@@())
+      val s = Seq(Match('x), Build('_@@('Zero@@(), 'x)))
       assertDomT(
-        lift(l.map(_ => Trm.App('Zero))))(
+        lift(l.map(elem => Trm.App('_, Trm.App('Zero), Trm.App(Symbol(s"Elem_${l.length - l.indexOf(elem) - 1}"))))))(
         analysis.analyze(Call('map, scala.List(s), scala.List()), lift(l), DEFS ++ Num.DEFS))
     }
   }

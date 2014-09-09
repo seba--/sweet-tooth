d81e26fb85103cf6aac9899f136f7686f85a7a7c
Sebastian Erdweg <seba.github@gmail.com>
2014-09-09 17:22:52 +0200

    deep meet and compare

--- src/main/scala/org/sugarj/sweettooth/stratego/analysis/domain/PowersetDomain.scala
+++ src/main/scala/org/sugarj/sweettooth/stratego/analysis/domain/PowersetDomain.scala
@@ -26,16 +26,28 @@ object PowersetDomain {
     def compare(morePrecise: T, lessPrecise: T): Boolean = (morePrecise, lessPrecise) match {
       case (_,None) => true
       case (None,_) => false
-      case (Some(s1), Some(s2)) => s1 subsetOf s2
+      case (Some(s1), Some(s2)) =>
+        def findInS2(t1: Trm): Boolean = t1 match {
+          case t1: Lit[_] => s2.contains(t1)
+          case App(cons1, args1) =>
+            s2 map {
+              case App(`cons1`, args2) if args1.size == args2.size =>
+                if (args1.zip(args2).forall{case (a1, a2) => compare(a1, a2)})
+                  return true
+            }
+            false
+        }
+
+        s1.forall(findInS2(_))
     }
 
     def join(t1: T, t2: T): T = (t1,t2) match {
       case (None,_) => None
       case (_,None) => None
-      case (Some(s1), Some(s2)) => Some(merge(s1, s2))
+      case (Some(s1), Some(s2)) => Some(mergeUnion(s1, s2))
     }
 
-    def merge(s1: Set[Trm], s2: Set[Trm]) = {
+    def mergeUnion(s1: Set[Trm], s2: Set[Trm]) = {
       var lits = Set[Trm]()
       var apps = Map[Symbol, List[T]]()
 
@@ -59,7 +71,47 @@ object PowersetDomain {
     def meet(t1: T, t2: T): T = (t1,t2) match {
       case (None,_) => t2
       case (_,None) => t1
-      case (Some(s1), Some(s2)) => Some(s1 intersect s2)
+      case (Some(s1), Some(s2)) => Some(mergeMeet(s1, s2))
+    }
+
+    def mergeMeet(s1: Set[Trm], s2: Set[Trm]) = {
+      type M = collection.mutable.Map[Symbol, List[T]]
+
+      var lits = Set[Trm]()
+      def makeJoinedMap(s: Set[Trm], m: M) = s.map(t => t match {
+        case l: Lit[_] => lits += l
+        case App(cons, xs) =>
+          m.get(cons) match {
+            case None => m += cons -> xs
+            case Some(ys) =>
+              val args = ys zip xs
+              val joined = args.map(p => join(p._1,p._2))
+              m += cons -> joined
+          }
+      })
+
+      var apps = Map[Symbol, List[T]]()
+      def makedMetMap(m1: M, m2: M) = m2.map(t => t match {
+        case (cons, xs) =>
+          m1.get(cons) match {
+            case None => // no meet
+            case Some(ys) =>
+              val args = ys zip xs
+              val met = args.map(p => meet(p._1,p._2))
+              if (!met.forall(_.isEmpty))
+                apps += cons -> met
+          }
+      })
+
+      var apps1 = collection.mutable.Map[Symbol, List[T]]()
+      makeJoinedMap(s1, apps1)
+
+      lits = Set()
+      var apps2 = collection.mutable.Map[Symbol, List[T]]()
+      makeJoinedMap(s2, apps2)
+
+      makedMetMap(apps1, apps2)
+      lits ++ apps.map(p => App(p._1,p._2))
     }
 
     def matchAppPat(cons: Symbol, arity: Int, t: T): Set[List[T]] = t match {
